0cba5a3f7946b99b15ae23e04de7b8d52858d909
Michael Jackson <mike.jackson@bluequartz.net>
2019-09-03 14:28:19 -0400

    Updating codes to latest API changes to the Orientation Transformation codes.
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- DREAM3DReviewFilters/FindCSLBoundaries.cpp
+++ DREAM3DReviewFilters/FindCSLBoundaries.cpp
@@ -55,10 +55,10 @@
 #include "SIMPLib/Geometry/TriangleGeom.h"
 #include "SIMPLib/Math/GeometryMath.h"
 #include "SIMPLib/Math/MatrixMath.h"
-#include "SIMPLib/Math/QuaternionMath.hpp"
+
 #include "SIMPLib/Math/SIMPLibMath.h"
 
-#include "OrientationLib/OrientationMath/OrientationTransforms.hpp"
+
 
 #include "DREAM3DReview/DREAM3DReviewConstants.h"
 #include "DREAM3DReview/DREAM3DReviewVersion.h"
@@ -124,9 +124,9 @@ public:
     QuatType s1_misq;
     QuatType s2_misq;
 
-    QuatF* quats = reinterpret_cast<QuatF*>(m_Quats);
-
-    double xstl_norm[3], s_xstl_norm[3], cslAxisNorm[3];
+    double xstl_norm[3];
+    std::array<double, 3> s_xstl_norm;
+    double cslAxisNorm[3];
     double cslAxisNormDenom = 0.0;
     cslAxisNormDenom =
         sqrtf(TransformationPhaseConstants::CSLAxisAngle[m_CSLIndex][2] + TransformationPhaseConstants::CSLAxisAngle[m_CSLIndex][3] + TransformationPhaseConstants::CSLAxisAngle[m_CSLIndex][4]);
@@ -146,37 +146,32 @@ public:
       if(feature1 > 0 && feature2 > 0) // && m_Phases[feature1] != m_Phases[feature2])
       {
         w = 10000.0;
-
-        QuatType q1 = QuaternionMathType::FromType<float>(quats[feature1]);
-        QuatType q2 = QuaternionMathType::FromType<float>(quats[feature2]);
+        float* quatPtr = m_Quats + feature1 * 4;
+        QuatType q1(quatPtr[0], quatPtr[1], quatPtr[2], quatPtr[3]);
+        quatPtr = m_Quats + feature2 * 4;
+        QuatType q2(quatPtr[0], quatPtr[1], quatPtr[2], quatPtr[3]);
 
         phase1 = m_CrystalStructures[m_Phases[feature1]];
         phase2 = m_CrystalStructures[m_Phases[feature2]];
         if(phase1 == phase2)
         {
           int nsym = m_OrientationOps[phase1]->getNumSymOps();
-          QuaternionMathType::Conjugate(q2);
-          QuaternionMathType::Multiply(q1, q2, misq);
-          OrientArrayType om(9);
-          OrientTransformsType::qu2om(OrientArrayType(q1), om);
-          om.toGMatrix(g1);
+          misq = q1 * (q2.conjugate());
+          OrientationTransformation::qu2om<QuatType, OrientationD>(q1).toGMatrix(g1);
           MatrixMath::Multiply3x3with3x1(g1, normal, xstl_norm);
           for(int j = 0; j < nsym; j++)
           {
-            m_OrientationOps[phase1]->getQuatSymOp(j, sym_q);
+            sym_q = m_OrientationOps[phase1]->getQuatSymOp(j);
             // calculate crystal direction parallel to normal
-            QuaternionMathType::Multiply(misq, sym_q, s1_misq);
-            QuaternionMathType::MultiplyQuatVec(sym_q, xstl_norm, s_xstl_norm);
+            s1_misq = misq * sym_q;
+            s_xstl_norm = sym_q.multiplyByVector(xstl_norm);
             for(int k = 0; k < nsym; k++)
             {
               // calculate the symmetric misorienation
-              m_OrientationOps[phase1]->getQuatSymOp(k, sym_q);
-              QuaternionMathType::Conjugate(sym_q);
-              QuaternionMathType::Multiply(sym_q, s1_misq, s2_misq);
+              sym_q = m_OrientationOps[phase1]->getQuatSymOp(k);
+              s2_misq = sym_q.conjugate() * s1_misq;
 
-              OrientArrayType ax(4);
-              OrientTransformsType::qu2ax(OrientArrayType(s2_misq), ax);
-              ax.toAxisAngle(n1, n2, n3, w);
+              OrientationTransformation::qu2ax<QuatType, OrientationD>(s2_misq).toAxisAngle(n1, n2, n3, w);
               w = w * 180.0 / SIMPLib::Constants::k_Pi;
               axisdiffCSL = std::acosf(fabs(n1) * cslAxisNorm[0] + std::fabs(n2) * cslAxisNorm[1] + std::fabs(n3) * cslAxisNorm[2]);
               angdiffCSL = std::fabs(w - TransformationPhaseConstants::CSLAxisAngle[m_CSLIndex][1]);
@@ -186,7 +181,7 @@ public:
                 n[1] = n2;
                 n[2] = n3;
                 m_CSLBoundary[i] = true;
-                incoherence = 180.0 * acos(GeometryMath::CosThetaBetweenVectors(n, s_xstl_norm)) / SIMPLib::Constants::k_Pi;
+                incoherence = 180.0 * acos(GeometryMath::CosThetaBetweenVectors(n, s_xstl_norm.data())) / SIMPLib::Constants::k_Pi;
                 if(incoherence > 90.0)
                 {
                   incoherence = 180.0 - incoherence;
--- DREAM3DReviewFilters/InsertTransformationPhases.cpp
+++ DREAM3DReviewFilters/InsertTransformationPhases.cpp
@@ -59,9 +59,9 @@
 #include "SIMPLib/Math/SIMPLibMath.h"
 #include "SIMPLib/Math/SIMPLibRandom.h"
 
+#include "OrientationLib/Core/Orientation.hpp"
 #include "OrientationLib/LaueOps/LaueOps.h"
-#include "OrientationLib/OrientationMath/OrientationArray.hpp"
-#include "OrientationLib/OrientationMath/OrientationTransforms.hpp"
+
 
 #include "DREAM3DReview/DREAM3DReviewConstants.h"
 #include "DREAM3DReview/DREAM3DReviewVersion.h"
@@ -703,10 +703,8 @@ void InsertTransformationPhases::insertTransformationPhases()
         }
       }
       // find where the habit plane points
-      QuaternionMathF::Copy(avgQuats[curFeature], q1);
-      FOrientArrayType om(9);
-      FOrientTransformsType::qu2om(FOrientArrayType(q1), om);
-      om.toGMatrix(g);
+      QuatF q1(m_AvgQuats + curFeature * 4);
+      OrientationTransformation::qu2om<QuatF, OrientationF>(q1).toGMatrix(g);
       MatrixMath::Transpose3x3(g, gT);
       MatrixMath::Multiply3x3with3x1(gT, crystalHabitPlane, sampleHabitPlane);
 
@@ -721,8 +719,7 @@ void InsertTransformationPhases::insertTransformationPhases()
       //    rotMat[2][2]=0;
 
       // generate transformation phase orientation with a user-defined rotation about the habit plane
-      FOrientTransformsType::ax2om(FOrientArrayType(crystalHabitPlane[0], crystalHabitPlane[1], crystalHabitPlane[2], sig3), om);
-      om.toGMatrix(rotMat);
+      OrientationTransformation::ax2om<OrientationF, OrientationF>(OrientationF(crystalHabitPlane[0], crystalHabitPlane[1], crystalHabitPlane[2], sig3)).toGMatrix(rotMat);
       MatrixMath::Multiply3x3with3x3(rotMat, g, newMat);
 
       // find the minimum angle
@@ -748,12 +745,9 @@ void InsertTransformationPhases::insertTransformationPhases()
       // assign our symmetry matrix to that which produced the minimum angle
       orientOps->getMatSymOp(minPos, symMat);
       MatrixMath::Multiply3x3with3x3(symMat, newMatCopy, newMat);
-      FOrientArrayType eOut(e, 3);
-      FOrientTransformsType::om2eu(FOrientArrayType(newMat), eOut);
+      OrientationF eOut = OrientationTransformation::om2eu<OrientationF, OrientationF>(OrientationF(symMat));
 
-      FOrientArrayType quat(4);
-      FOrientTransformsType::eu2qu(eOut, quat);
-      q2 = quat.toQuaternion<float>();
+      QuatF q2 = OrientationTransformation::eu2qu<OrientationF, QuatF>(eOut);
 
       // define plate = user input fraction of eq dia centered at centroid
       // NOTE: we multiply by 0.5 because the transformation phase thickness will be established by
@@ -1066,7 +1060,7 @@ size_t InsertTransformationPhases::transferAttributes(size_t totalFeatures, Quat
   m_AvgQuats[4 * totalFeatures + 0] = q[0];
   m_AvgQuats[4 * totalFeatures + 1] = q[1];
   m_AvgQuats[4 * totalFeatures + 2] = q[2];
-  m_AvgQuats[4 * totalFeatures + 3] = q.w;
+  m_AvgQuats[4 * totalFeatures + 3] = q[3];
   m_Centroids[3 * totalFeatures + 0] = 1.0f;
   m_Centroids[3 * totalFeatures + 1] = 2.0f;
   m_Centroids[3 * totalFeatures + 2] = 3.0f;
